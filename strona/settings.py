794aefc0db84f3120f1f05ff0136946ce7f6b9d3
retarf <retarf@gmail.com>
2016-08-01 17:01:13 +0200

    seciurity update

--- strona/settings.py
+++ strona/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for strona project.
 
@@ -11,6 +12,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+
+from locale import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,18 +79,14 @@ WSGI_APPLICATION = 'strona.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'magdoc',
-        'USER': 'retarf',
-        'PASSWORD': 'meggie997',
-        'HOST': 'localhost',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
 
 
 # Password validation
