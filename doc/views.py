419631307cbdf51698fa151ce6c9e8118cee275b
retarf <retarf@gmail.com>
2016-08-02 11:51:41 +0200

    check

--- doc/views.py
+++ doc/views.py
@@ -5,12 +5,12 @@ from .models import Doc, Client
 # Create your views here.
 
 def lists(request):
-	all_doc = Doc.objects.order_by('number').all()
-	all_clients = Client.objects.all()
-	return render(request, 'doc/index.html',
-		{'all_doc' : all_doc,
-		'all_clients' : all_clients,
-		})
+ all_doc = Doc.objects.order_by('number').all()
+ all_clients = Client.objects.all()
+ return render(request, 'doc/index.html',
+  {'all_doc' : all_doc,
+  'all_clients' : all_clients,
+  })
 
 def detail(request, doc_id):
-	return HttpResponse("<h1> Szczegoly dla " + str(doc_id) + " <h1>")
+ return HttpResponse("<h1> Szczegoly dla " + str(doc_id) + " <h1>")
